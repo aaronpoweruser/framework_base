cc1159e66070cbfedbbe8f6c4491e248b4122057
agascoig <user@server>
2012-09-11 11:15:27 -0400

    fix aliasing violatoins

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -77,6 +77,7 @@ LOCAL_SRC_FILES:= \
 	android_net_TrafficStats.cpp \
 	android_net_wifi_Wifi.cpp \
 	android_nio_utils.cpp \
+	android_util_PackageRedirectionMap.cpp \
 	android_text_format_Time.cpp \
 	android_util_AssetManager.cpp \
 	android_util_Binder.cpp \
